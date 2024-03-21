f9f5de69625f911998fc4581948043042f89921b
ShiiBuukii <shigure.to.fubuki@gmail.com>
2024-03-21 16:36:43 +0700

    added core

---
+++ src/core/errors/authError.ts
@@ -0,0 +1,30 @@
+
+export class RegisterError extends Error{
+    constructor(message: string){
+        super(message)
+    }
+}
+
+export class LoginError extends Error{
+    constructor(message: string){
+        super(message)
+    }
+}
+
+export class ProductError extends Error{
+    constructor(message: string){
+        super(message)
+    }
+}
+
+export class UnauthorizedError extends Error{
+    constructor(message:string){
+        super(message)
+    }
+}
+
+export class InvalidRequestError extends Error{
+    constructor(message: string){
+        super(message)
+    }
+}
